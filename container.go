5a6411e695e36867918d7f77e1f9a0f25b6532cf
root <shayan1337@gmail.com>
2020-04-16 13:46:04 +0530

    demo for creating child process with pid from new namespace

--- container.go
+++ container.go
@@ -12,6 +12,8 @@ func main() {
   switch os.Args[1] {
   case "run":
     run()
+  case "child":
+    child()
   default:
     panic("invalid command!!!")
   }
@@ -20,21 +22,35 @@ func main() {
 func run() {
   fmt.Printf("Running %v as PID %d \n", os.Args[2:], os.Getpid())
 
-  cmd := exec.Command(os.Args[2], os.Args[3:]...)
+  args := append([]string{"child"}, os.Args[2:]...)
+
+  cmd := exec.Command("/proc/self/exe", args...)
 
   cmd.Stdin = os.Stdin
   cmd.Stdout= os.Stdout
   cmd.Stderr = os.Stderr
 
   cmd.SysProcAttr = &syscall.SysProcAttr{
-    Cloneflags: syscall.CLONE_NEWUTS,
+    Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWPID,
   }
 
   must(cmd.Run())
 }
 
+func child() {
+  fmt.Printf("Running %v as PID %d \n", os.Args[2:], os.Getpid())
+
+  cmd := exec.Command(os.Args[2], os.Args[3:]...)
+
+  cmd.Stdin = os.Stdin
+  cmd.Stdout= os.Stdout
+  cmd.Stderr = os.Stderr
+
+  must(cmd.Run())
+}
+
 func must(err error) {
-    if err !=nil {
-        panic(err)
-    }
+  if err != nil {
+    panic(err)
+  }
 }
